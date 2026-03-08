752183bfc18d997abeef6f809c709d824e00a5ca
Shafkatahmad <shafkataahmad@gmail.com>
2026-03-08 23:18:15 +0600

    pricing card

---
+++ src/Components/PricingOptions/PricingOptions.jsx
@@ -0,0 +1,19 @@
+import { use } from "react";
+import PricingCard from "../PricingCard/PricingCard";
+
+const PricingOptions = ({pricingPromise}) => {
+  const pricingData = use(pricingPromise)
+  
+  return (
+    <div>
+      <h2 className="text-5xl">Our Membership</h2>
+      <div className="grid md:grid-cols-3 gap-8">
+        {
+          pricingData.map(pricing => <PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+        }
+      </div>
+    </div>
+  );
+};
+
+export default PricingOptions;
